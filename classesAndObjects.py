5474eac8bfadfb0c861dbf8e8972cf7af31888e5
unknown <kalyan14.marolix@gmail.com>
2023-11-01 12:00:23 +0530

    task completed

---
+++ classesAndObjects.py
@@ -0,0 +1,45 @@
+class school:
+    def student(self):
+        print("I'm student")
+    
+    def teacher(self):
+        print("I'm teacher")
+    
+    def peon(self):
+        print("I'm peon")
+    
+    def principal(self):
+        print("I'm principal")
+    
+    def vice_principal(self):
+        print("I'm vice principal")
+
+obj1=school()
+obj1.student()
+
+obj2=school()
+obj2.student()
+
+obj3=school()
+obj3.teacher()
+
+obj4=school()
+obj4.teacher()  
+       
+obj5=school()
+obj5.peon() 
+
+obj6=school()
+obj6.peon()  
+
+obj7=school()
+obj7.principal()
+
+obj8=school()
+obj8.principal()  
+
+obj9=school()
+obj9.vice_principal()  
+
+obj10=school()
+obj10.vice_principal() 
